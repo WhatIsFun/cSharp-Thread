9d67dcb8405f00cab0f1df826c468fed5617b4f4
WhatIsFun <mohammed110051@gmail.com>
2023-07-23 11:36:16 +0400

    Foreground and BackGround

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
             Thread thread = new Thread(new ThreadStart(doSomeWork));
+            thread.IsBackground = true; // To use BackGround thread
             thread.Start();
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Main Thread: {0}", i);
                 Thread.Sleep(10);
             }
-            thread.Join();
+            thread.Join(); // Join use here to not stop the doSomeWork thread from debugging.
             Console.WriteLine("Main Block");
         }
 
@@ -20,7 +21,7 @@
             for(int i = 0;i < 10; i++)
             {
                 Console.WriteLine("Worker: {0}", i);
-                Thread.Sleep(2000);
+                Thread.Sleep(100);
             }
         }
     }
