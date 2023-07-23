0dfc1c7b1bf9857131323fccd2288d224907342a
WhatIsFun <mohammed110051@gmail.com>
2023-07-23 10:58:25 +0400

    cSharp-Thread

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace cSharp_Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(doSomeWork));
+            thread.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main Thread: {0}", i);
+                Thread.Sleep(10);
+            }
+            thread.Join();
+            Console.WriteLine("Main Block");
+        }
+
+        public static void doSomeWork()
+        {
+            for(int i = 0;i < 10; i++)
+            {
+                Console.WriteLine("Worker: {0}", i);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
